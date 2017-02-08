d2eecce4000417505482264a72ad8e9c47579a0c
avnitalati <avnitalati@gmail.com>
2017-02-08 22:46:45 +0530

    Fragment commited

---
+++ app/src/main/java/com/example/hp/fragment_demo3/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.hp.fragment_demo3;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fm=getSupportFragmentManager();
+        FragmentTransaction ft=fm.beginTransaction();
+        Fragment1 fragment1 =new Fragment1();
+        ft.add(R.id.l1,fragment1 );
+        ft.commit();
+    }
+}
